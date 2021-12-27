6abf0aee44c9bb8ea5e3493ddeb803db3bbecb7f
Manuel Duarte <carlosd99m@gmail.com>
2021-12-27 03:17:27 -0600

    chore: configuracion basica

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,32 @@
+import expres from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+// initialize express
+const app = expres()
+
+/**
+ * Setings
+ */
+app.set('port', process.env.PORT || 3000)
 
 
+/**
+ * Middleware
+ */
+app.use(morgan('dev'))
+app.use(expres.urlencoded({ extended: false }))
+app.use(expres.json())
+app.use(cors())
+
+
+/**
+ * Routes
+ */
+
+app.get('/', (req, res) => {
+  res.send(`The API is at http://localhost:${app.get('port')}`)
+})
+
+
+export default app
